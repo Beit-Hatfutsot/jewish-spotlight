610d809bbeb55dc5308e4e1ed4067aaa090df00b
Nir Goldberg <nir@htmline.com>
2016-07-08 10:33:04 +0300

    Photo data type - template structure

--- bhjs-content/themes/bhjs/view/data-types/photo.php
+++ bhjs-content/themes/bhjs/view/data-types/photo.php
@@ -11,10 +11,50 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $lang, $data;
 
+$settings = array(
+	'type_id'		=> 1,
+	'src_prefix'	=> 'https://storage.googleapis.com/bhs-flat-pics/'
+);
+
 ?>
 
 <div class="data-type-section" id="data-type-section-photo">
 
+	<div id="gallery-1" class="gallery">
 
+		<?php foreach ( $data[ $settings['type_id'] ] as $collection ) {
+
+			$title	= $collection['Header'][ucfirst($lang)];
+			$desc	= $collection['UnitText1'][ucfirst($lang)];
+			$photos	= array();
+
+			if ( count( $collection['Pictures'] ) ) {
+				foreach ( $collection['Pictures'] as $photo ) {
+
+					if ( isset( $photo['PictureId'] ) ) {
+						$photos[] = $photo['PictureId'] . '.jpg';
+					}
+				}
+			}
+
+			if ( count( $photos ) ) {
+
+				$index = 1;
+
+				foreach ( $photos as $photo ) { ?>
+
+					<figure class="gallery-item">
+						<a href="<?php echo $settings['src_prefix'] . $photo; ?>">
+							<img src="<?php echo $settings['src_prefix'] . $photo; ?>" width="285" height="auto" alt="<?php echo $title . ( count($photos) > 1 ? ' - ' . $index++ : '' ); ?>" />
+						</a>
+					</figure>
+
+				<?php }
+
+			}
+
+		} ?>
+
+	</div>
 
 </div>
