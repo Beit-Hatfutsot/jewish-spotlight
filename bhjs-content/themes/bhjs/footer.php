a210ef048f72a3d43028ea031304f42b9052011c
Ori Hoch <ori@uumpa.com>
2019-04-04 13:22:28 +0300

    refresh js/css assets cache

--- bhjs-content/themes/bhjs/footer.php
+++ bhjs-content/themes/bhjs/footer.php
@@ -31,7 +31,7 @@
 	<script src="<?php echo JS_DIR; ?>/lib/jquery.waypoints.min.js"></script>
 
 	<!-- general.min.js -->
-	<script src="<?php echo JS_DIR; ?>/general.min.js?v=3"></script>
+	<script src="<?php echo JS_DIR; ?>/general.min.js?v=4"></script>
 
 	<?php include( TEMPLATEPATH . '/view/footer/footer-photoswipe.php' ); ?>
 
